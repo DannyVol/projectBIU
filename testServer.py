3cfdbaad5f0aa8f790d53289ab1041ed3b2f396e
DannyVol <69720882+DannyVol@users.noreply.github.com>
2023-02-18 03:10:09 +0200

    add loop
    new format

--- testServer.py
+++ testServer.py
@@ -1,13 +1,17 @@
 import requests
 import json
+import os
+import time
 
+address = os.popen("minikube kubectl -- get services --namespace default flask-service --output jsonpath='{.status.loadBalancer.ingress[0].ip}'").read()
 body = {
     "Content-Type": "application/json",
     "content": "ping"
 }
 
-pathUri = 'http://localhost:5005/pingpong'
-request = requests.get(pathUri, json=body, verify=False,)
-#request = requests.get('https://rafis-vra.terasky.local{}'.format(pathUri), verify=False, headers=header)
-content = json.loads(request.text)
-print(content)
+while True:
+    pathUri = 'http://' + address + ':5005/pingpong'
+    request = requests.get(pathUri, json=body, verify=False,)
+    content = json.loads(request.text)
+    print(str(pathUri) + str(content) + " Ding Dong digi dong my k8s very long...")
+    time.sleep(5)
